923451f0c33eb485049667b62ab4b0366e41b027
Jan Wozniak <wozniak.jan@gmail.com>
2016-08-14 19:01:45 -0400

    ps1 print 'default'

--- usr/share/ev/completion/ev-prompt.sh
+++ usr/share/ev/completion/ev-prompt.sh
@@ -95,7 +95,7 @@ __ev_update_path()
 __ev_ps1 ()
 {
     local active=`ev set 2> /dev/null`
-    if [ "$active" != default ] && [ ! -z "$active" ]; then
+    if [ ! -z "$active" ]; then
         echo "${1}${active}${2}"
     fi
 }
